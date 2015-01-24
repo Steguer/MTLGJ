82a37d161d0961b1cf31eb91c58178bbc21c412a
yvanovitch <yvan.richer@gmail.com>
2015-01-24 18:46:01 -0500

    Ajout images Ajout scène de test trou Gestion des trous

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -5,27 +5,29 @@ using System.Collections;
 
 public class Playermovement : MonoBehaviour
 {
-		public float movementSpeed = 5.0f;
-		public string player = "1";
-		public bool isFalling = false;
-		float fallSpeed = 3f;
-		public Vector3 fallingIn;
-		float movingTowardsTrapSpeed = 1f;
-		protected Animator animator;
-		void Start ()
+	public float movementSpeed = 5.0f;
+	public string player = "1";
+	public bool isFalling = false;
+	public bool isFlying = false;
+	float fallSpeed = 3f;
+	public Vector3 fallingIn;
+	float movingTowardsTrapSpeed = 1f;
+	protected Animator animator;
+	void Start ()
+	{
+		animator = GetComponent<Animator> ();
+		animator.SetInteger ("direction", 0);
+		animator.speed = 0;
+	}
+	void Update ()
+	{
+		if (isFalling && (transform.localScale.x > 0f))
 		{
-				animator = GetComponent<Animator> ();
-				animator.SetInteger ("direction", 0);
-				animator.speed = 0;
+			Debug.Log("And he falls");
+			transform.localScale -= new Vector3(fallSpeed * Time.deltaTime, fallSpeed * Time.deltaTime, 0f);
+			transform.position = Vector3.MoveTowards(transform.position, fallingIn, movingTowardsTrapSpeed * Time.deltaTime);
+			return;
 		}
-		void Update ()
-		{
-				if (isFalling && (transform.localScale.x > 0f)) {
-						Debug.Log ("And he falls");
-						transform.localScale -= new Vector3 (fallSpeed * Time.deltaTime, fallSpeed * Time.deltaTime, 0f);
-						transform.position = Vector3.MoveTowards (transform.position, fallingIn, movingTowardsTrapSpeed * Time.deltaTime);
-						return;
-				}
 				//rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, 0); //Set X and Z velocity to 0
 
 				if (Input.GetJoystickNames ().Length >= int.Parse (player)) {
@@ -36,23 +38,17 @@ public class Playermovement : MonoBehaviour
 						float inputY = Input.GetAxis ("VerticalPlayer_" + player);
 			
 						animator.speed = 1;
-						bool isRunning = false;
-			
-						if (inputY > 0) {
-								isRunning = true;
+						if (inputY > 0) {//haut
 								animator.SetInteger ("direction", 1);			
 						} else if (inputY < 0) {//bas
-								isRunning = true;
 								animator.SetInteger ("direction", 2);
 						} else if (inputX < 0) {//gauche
-								isRunning = true;
 								animator.SetInteger ("direction", 3);
 						} else if (inputX > 0) {//droite
-								isRunning = true;
 								animator.SetInteger ("direction", 4);
+						} else {
+								animator.speed = 0;
 						}
-
-						animator.SetBool ("isRunning", isRunning);
 			
 						transform.Translate (Input.GetAxis ("HorizontalPlayer_" + player) * Time.deltaTime * movementSpeed, 0, 0);
 						transform.Translate (0, Input.GetAxis ("VerticalPlayer_" + player) * Time.deltaTime * movementSpeed, 0);
@@ -134,64 +130,64 @@ public class Playermovement : MonoBehaviour
 				
 				
 						} else if (int.Parse (player) == 2) {
-								bool isRunning = false;
-								animator.speed = 1;
+				bool isRunning = false;
+				animator.speed = 1;
 				
-								if (Input.GetKey (KeyCode.UpArrow)) {
-										animator.SetInteger ("direction", 1);
-										isRunning = true;
-								}
+				if (Input.GetKey (KeyCode.UpArrow)) {
+					animator.SetInteger ("direction", 1);
+					isRunning = true;
+				}
 				
-								if (Input.GetKey (KeyCode.RightArrow)) {
-										isRunning = true;
-										animator.SetInteger ("direction", 4);
-								} 
+				if (Input.GetKey (KeyCode.RightArrow)) {
+					isRunning = true;
+					animator.SetInteger ("direction", 4);
+				} 
 				
-								if (Input.GetKey (KeyCode.DownArrow)) {
-										isRunning = true;
-										animator.SetInteger ("direction", 2);
-								}
+				if (Input.GetKey (KeyCode.DownArrow)) {
+					isRunning = true;
+					animator.SetInteger ("direction", 2);
+				}
 				
-								if (Input.GetKey (KeyCode.LeftArrow)) {
-										isRunning = true;
-										animator.SetInteger ("direction", 3);
-								}
+				if (Input.GetKey (KeyCode.LeftArrow)) {
+					isRunning = true;
+					animator.SetInteger ("direction", 3);
+				}
 				
-								animator.SetBool ("isRunning", isRunning);
+				animator.SetBool ("isRunning", isRunning);
 				
 				
-								Debug.Log (isRunning);
+				Debug.Log (isRunning);
 				
-								if (isRunning) {
-										switch (animator.GetInteger ("direction")) {
-										case 1:
-												transform.Translate (0, 1 * Time.deltaTime * movementSpeed, 0);
-												break;
-										case 2:
-												transform.Translate (0, -1 * Time.deltaTime * movementSpeed, 0);
-												break;
-										case 3:
-												transform.Translate (-1 * Time.deltaTime * movementSpeed, 0, 0);
-												break;		
-										case 4:
-												transform.Translate (1 * Time.deltaTime * movementSpeed, 0, 0);
-												break;
-										default:
-												break;
+				if (isRunning) {
+					switch (animator.GetInteger ("direction")) {
+					case 1:
+						transform.Translate (0, 1 * Time.deltaTime * movementSpeed, 0);
+						break;
+					case 2:
+						transform.Translate (0, -1 * Time.deltaTime * movementSpeed, 0);
+						break;
+					case 3:
+						transform.Translate (-1 * Time.deltaTime * movementSpeed, 0, 0);
+						break;		
+					case 4:
+						transform.Translate (1 * Time.deltaTime * movementSpeed, 0, 0);
+						break;
+					default:
+						break;
 						
-										}
-								}
-								if (Input.GetKeyDown ("[1]")) {
-										Debug.Log ("suicide of " + player);
-								}
+					}
+				}
+				if (Input.GetKeyDown ("[1]")) {
+					Debug.Log ("suicide of " + player);
+				}
 				
-								if (Input.GetKeyDown ("[2]")) {
-										Debug.Log ("action of " + player);
-								}
+				if (Input.GetKeyDown ("[2]")) {
+					Debug.Log ("action of " + player);
+				}
 				
-								if (Input.GetKeyDown ("[3]")) {
-										Debug.Log ("pause of " + player);
-								}
+				if (Input.GetKeyDown ("[3]")) {
+					Debug.Log ("pause of " + player);
+				}
 				
 				
 
@@ -208,6 +204,14 @@ public class Playermovement : MonoBehaviour
 				// 5 - Move the game object
 				//rigidbody2D.velocity = movement;
 		}
-	
+
+	public void setFalling (bool isFalling, Vector3 position)
+	{
+		if (isFlying)
+			return;
+
+		this.isFalling = isFalling;
+		this.fallingIn = position;
+	}
 	
 }
