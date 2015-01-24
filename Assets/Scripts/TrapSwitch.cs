493a81cf52f279dd1f8e07a950f72e4589a431cc
Kounga Leblanc <kevin.leblanc.1@etsmtl.net>
2015-01-24 13:37:34 -0500

    A lotta stuff

---
+++ Assets/Scripts/TrapSwitch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+    public bool state = false;
+    public SwitchEvent parent;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D collider2d) 
+    {
+        Debug.Log("Entering");
+        parent.activateEvent();
+    }
+
+    void OnTriggerStay2D(Collider2D collider2d) {
+
+    }
+
+    void OnTriggerExit2D(Collider2D collider2d)
+    {
+        Debug.Log("Exiting");
+        parent.deactivateEvent();
+    }
+}
