23efdc80d4b39e8702a700bc840c271c3ab2b12e
corentin-R <corentinraoult@laposte.net>
2015-01-24 01:09:30 +0100

    animation player

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -12,6 +12,14 @@ public class PlayerScript : MonoBehaviour {
 	
 	// 2 - Store the movement
 	private Vector2 movement;
+
+	protected Animator animator;
+
+	void Start () 
+	{
+
+		animator = GetComponent<Animator>();
+	}
 	
 	void Update()
 	{
@@ -24,7 +32,30 @@ public class PlayerScript : MonoBehaviour {
 			speed.x * inputX,
 			speed.y * inputY);
 
+		setAllFalse ();
+		if (inputY > 0) {//haut
+						animator.SetBool ("IsUp", true);			
+				} else if (inputY < 0) {//bas
+						animator.SetBool ("IsDown", true);
+				} else if (inputX < 0) {//gauche
+						animator.SetBool ("IsLeft", true);
+				} else if (inputX > 0) {//droite
+						animator.SetBool ("IsRight", true);
+				} else {
+					animator.speed=0;
+				}
 
+	}
+
+	void animate(){
+		}
+
+	void setAllFalse(){
+		animator.SetBool ("IsUp", false);
+		animator.SetBool ("IsDown", false);
+		animator.SetBool ("IsLeft", false);
+		animator.SetBool ("IsRight", false);
+		animator.speed=1;
 	}
 	
 	void OnDestroy()
