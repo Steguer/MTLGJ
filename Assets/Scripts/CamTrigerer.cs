642cd548933e4e44fca404e57c37f6070ac37706
Steven GERARD <steven.gerard.deruelle@gmail.com>
2015-01-24 13:53:58 -0500

    to change of room

--- Assets/Scripts/CamTrigerer.cs
+++ Assets/Scripts/CamTrigerer.cs
@@ -5,6 +5,9 @@ public class CamTrigerer : MonoBehaviour {
 
     public GameObject camera;
 	public GameObject beforeTile;
+	public int nbrPlayers = 2;
+	private int players = 0;
+	private bool enaSwitch = true;
 
     /* 0 = top
      * 1 = right
@@ -23,33 +26,46 @@ public class CamTrigerer : MonoBehaviour {
 		if (collider.gameObject.tag != "Player")
 			return;
 
-        if (position == 0)
-        {
-            camera.GetComponent<MoveCam>().moveUp();
-            this.position = 2;
-        }
-        else if (position == 1)
-        {
-            camera.GetComponent<MoveCam>().moveRight();
-            this.position = 3;
-        }
-        else if (position == 2)
-        {
-            camera.GetComponent<MoveCam>().moveDown();
-            this.position = 0;
-        }
-        else if (position == 3)
-        {
-            camera.GetComponent<MoveCam>().moveLeft();
-            this.position = 1;
-        }
-        else
-            Debug.Log("Invalid cam trigger 'position' variable");
+		if(players >= nbrPlayers && enaSwitch) {
+	        if (position == 0)
+	        {
+	            camera.GetComponent<MoveCam>().moveUp();
+	            this.position = 2;
+	        }
+	        else if (position == 1)
+	        {
+	            camera.GetComponent<MoveCam>().moveRight();
+	            this.position = 3;
+	        }
+	        else if (position == 2)
+	        {
+	            camera.GetComponent<MoveCam>().moveDown();
+	            this.position = 0;
+	        }
+	        else if (position == 3)
+	        {
+	            camera.GetComponent<MoveCam>().moveLeft();
+	            this.position = 1;
+	        }
+	        else
+	            Debug.Log("Invalid cam trigger 'position' variable");
 
-		collider2D.isTrigger = false;
+			Physics2D.IgnoreLayerCollision(9, 9, false);
+			beforeTile.collider2D.isTrigger = false;
+			enaSwitch = false;
+		}
     }
 
 	void OnTriggerEnter2D(Collider2D collider) {
-		beforeTile.collider2D.isTrigger = false;
+		//beforeTile.collider2D.isTrigger = false;
+		Physics2D.IgnoreLayerCollision(9, 9);
+		players++;
+		collider.gameObject.GetComponent<Playermovement>().movementSpeed = 0;
+	}
+
+	void OnTriggerStay2D(Collider2D collider) {
+		if(players >= nbrPlayers) {
+			collider.gameObject.GetComponent<Playermovement>().movementSpeed = 5.0f;
+		}
 	}
 }
