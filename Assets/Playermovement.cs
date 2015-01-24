4ddfa71343ff02ba0a5656d33a8aae869a7c67e4
Steven GERARD <steven.gerard.deruelle@gmail.com>
2015-01-24 04:38:25 -0500

    Add player controller

---
+++ Assets/Playermovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class Playermovement : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+	public string player;
+    private bool isGrounded = false;
+
+
+    void Update() {
+        //rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, 0); //Set X and Z velocity to 0
+ 
+        transform.Translate(Input.GetAxis("HorizontalPlayer_" + player) * Time.deltaTime * movementSpeed, 0, 0);
+		transform.Translate(0, Input.GetAxis("VerticalPlayer_" + player) * Time.deltaTime * movementSpeed, 0);
+		
+		if(Input.GetButtonDown("suicidePlayer_" + player)) {
+			Debug.Log("suicide of " + player);
+		}
+	}
+
+    void FixedUpdate()
+    {
+
+    }
+       
+
+}
